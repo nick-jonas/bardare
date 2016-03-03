a60a10725372dcb0b1601187bf42093aed03ebe8
nick-jonas <njonas45@gmail.com>
2016-03-03 00:00:39 -0500

    fixed server

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/app'));
 
 app.get('/', function(request, response) {
-  response.render('index');
+  response.sendFile(__dirname + '/app/index.html')
 });
 
 app.listen(app.get('port'), function() {
